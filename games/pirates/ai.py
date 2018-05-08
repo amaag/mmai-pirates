03dee09e2bb2c81ead5fa1a53b1f4a319ea56c15
amaag <adam.maag@altec.com>
2018-05-08 12:09:15 +0000

    first 'working' AI

--- games/pirates/ai.py
+++ games/pirates/ai.py
@@ -6,8 +6,282 @@ from joueur.base_ai import BaseAI
 # you can add additional import(s) here
 # <<-- /Creer-Merge: imports -->>
 
+
+
+
+class RoleBase():
+    
+    def __init__(self, AI, unit):
+        self.AI = AI
+        self.unit_id = unit.id
+    
+
+    def __str__(self):
+        return("Role: unit = {}".format(self.unit_id))
+
+
+    def fitness(AI, unit):
+        return 0
+        
+
+    def effectiveness(self):
+        return 0
+    
+    def execute(self):
+        return(True)
+
+    def get_unit(self):
+        return(self.AI.game.get_game_object(self.unit_id))
+        
+
+class RecoverRole(RoleBase):
+
+    def __init__(self, AI, unit):
+        super().__init__(AI, unit)
+        self.is_deposited = False
+
+    def __str__(self):
+        return("RecoverRole: unit = {}".format(self.get_unit().id))
+
+
+    def fitness(AI, unit):
+        
+        if((unit is None) or (unit.tile is None)):
+            return 0;
+        
+        path_to_port = AI.find_path(unit.tile, AI.player.port.tile, unit)
+        if(len(path_to_port) < (AI.ship_moves + 1)):
+            return(1-(unit.ship_health / AI.game.ship_health))
+            
+        else:
+            return 0
+
+
+    def effectiveness(self):
+        if(is_deposited):
+            return 1
+        else:
+            return 0
+    
+    def execute(self):
+        
+        unit = self.get_unit()
+
+        # if this role's unit is dead...
+        if(
+            (unit.tile is None) or
+            (unit.ship_health == 0) or
+            (unit.ship_health == self.AI.game.ship_health)
+        ):
+            # return that the role is completed
+            return True;
+
+        # don't block the port
+        if(unit.tile == self.AI.player.port.tile):
+            print("MOVING OFF PORT")
+            path = self.AI.find_path(self.AI.player.port.tile, self.AI.player.opponent.port.tile, unit)
+            if(len(path) > 0):
+                self.AI.move_to(unit, path[0])
+
+
+        if(self.AI.move_to(unit, self.AI.player.port.tile, 1)):
+            unit.rest()
+            if(unit.ship_health == self.AI.game.ship_health):
+                return True
+            else:
+                return False
+            
+        else:
+            return False
+
+
+class GoldRunnerRole(RoleBase):
+
+    def __init__(self, AI, unit):
+        super().__init__(AI, unit)
+        self.is_deposited = False
+
+    def __str__(self):
+        return("GoldRunnerRole: unit = {}".format(self.get_unit().id))
+
+
+    def fitness(AI, unit):
+        if(unit.gold == 0):
+            return 0
+            
+        else:
+            #TODO: should we consider crew health? distance from the port?
+            return(max(unit.gold / 600, 1) * (unit.ship_health / AI.game.ship_health))
+
+    def effectiveness(self):
+        if(is_deposited):
+            return 1
+        else:
+            return 0
+    
+    def execute(self):
+        
+        unit = self.get_unit()
+        
+        # if this role's unit is dead...
+        if(
+            (unit.tile is None) or
+            (unit.ship_health == 0)
+        ):
+            # return that the role is completed
+            return True;
+
+        if(self.AI.move_to(unit, self.AI.player.port.tile)):
+            unit.deposit()
+            return True
+            
+        else:
+            return False
+
+
+class ShipKillerRole(RoleBase):
+    
+    shipTargets = dict()
+    
+
+    def __init__(self, AI, unit):
+        super().__init__(AI, unit)
+        self.kill_count = 0
+        self.turn_count = 0
+        self.target_id = None
+
+    def __del__(self):
+        self.untarget()
+
+    def __str__(self):
+        return("ShipKillerRole: unit = {}, target = {}".format(self.get_unit().id, self.target_id))
+
+
+    def fitness(AI, unit):
+        
+        # unit is dead
+        if(unit.tile is None):
+            return 0
+          
+        # unit has no ship  
+        if(unit.ship_health == 0):
+            return 0
+            
+        # no target available
+        if(ShipKillerRole.get_best_target_for_unit(AI, unit) is None):
+            return 0
+            
+            
+        return 1
+        
+    def effectiveness(self):
+        if(self.turn_count == 0):
+            return 0
+        else:
+            return(self.kill_count / self.turn_count)
+    
+    
+    def execute(self):
+        unit = self.get_unit()
+        target = self.AI.game.get_game_object(self.target_id)
+        self.turn_count += 1
+
+        if(target is None):
+            target = self.get_best_target()
+            self.target(target)
+
+        if(target is not None):
+    
+            # Find a path to this merchant
+            # TODO: intercept path
+            if(self.AI.move_to(unit, target.tile, self.AI.game._ship_range)):
+    
+                unit.attack(target.tile, "ship")
+                unit.log("attacking: {}".format(target.tile))
+                
+            # if we killed the ship...
+            if(target.ship_health == 0):
+                self.target(None)
+                self.kill_count += 1
+
+                #return goal is complete
+                return(True)
+                
+            # otherwise, if we did not kill the ship
+            else:
+                # return goal not complete
+                return(False)
+                
+        else:
+            # nothing to target
+            return True
+            
+        
+
+    def get_best_target(self):
+        return(ShipKillerRole.get_best_target_for_unit(self.AI, self.get_unit()))
+
+    def get_best_target_for_unit(AI, unit):
+                                    # Look for a merchant ship
+        merchant = None
+        max_fitness = 0
+        for target in AI.game.units:
+            fitness = ShipKillerRole.target_fitness(AI, unit, target)
+            if(fitness > max_fitness):
+                max_fitness = fitness
+                merchant = target
+
+        return(merchant)
+        
+    def target_fitness(AI, unit, target):
+        # calculate distance func
+        # calculate penalty for being otherwise targeted
+        # calculate weighting for owner (i.e. player vs. merchant)
+
+        if(
+            ((target.target_port is None) and (target.owner != AI.player.opponent)) or
+            (target.ship_health == 0) or
+            (target.tile is None)
+        ):
+            return 0
+
+
+            
+        targetWeight = ShipKillerRole.shipTargets.get(target.id, 0)
+
+        return 1/(1+abs(targetWeight - 1))
+
+
+    def target(self, target):
+        
+        if(target is None):
+            self.untarget()
+            return
+        
+        if(self.target_id != target.id):
+            self.untarget()
+            ShipKillerRole.shipTargets[target.id] = ShipKillerRole.shipTargets.get(target.id, 0) + 1
+            target.log("ID: {}".format(target.id))
+            self.target_id = target.id
+    
+    def untarget(self):
+        if(self.target_id is not None):
+            ShipKillerRole.shipTargets[self.target_id] = ShipKillerRole.shipTargets.get(self.target_id, 1) - 1
+            self.target_id = None
+        
+
+
 class AI(BaseAI):
     """ The basic AI functions that are the same between games. """
+
+    def __init__(self, game):
+        BaseAI.__init__(self, game)
+
+        self.goals = dict()
+        self.last_turn = -1
+        self.phase = 0
+
+
 
     def get_name(self):
         """ This is the name you send to the server so your AI will control the player named this string.
@@ -43,6 +317,8 @@ class AI(BaseAI):
         # <<-- Creer-Merge: end -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
         # replace with your end logic
         # <<-- /Creer-Merge: end -->>
+
+
     def run_turn(self):
         """ This is called every time it is this AI.player's turn.
 
@@ -51,54 +327,187 @@ class AI(BaseAI):
         """
         # <<-- Creer-Merge: runTurn -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
         # Put your game logic here for runTurn
-        if len(self.player._units) == 0:
-            # Spawn a crew if we have no units
+        
+        if(self.last_turn != self.game.current_turn):
+            self.last_turn = self.game.current_turn
+            self.phase = 0
+
+        if(self.phase == 0):
+
+            for goal_id in self.goals:
+                print("GOAL[{}]: {}".format(goal_id, self.goals[goal_id]))
+                
+                
+            for unit in self.player.units:
+                
+    
+                if(
+                    (unit.id not in self.goals) or
+                    (self.goals[unit.id] is None)
+                ):
+                    # goals[unit.id] = None
+
+                    # CLAIMING FOR goldrunner
+    
+                    # if(unit.gold > 0):
+                    #     goals[unit.id] = "goldrunner"
+    
+    
+                    # CLAIMING FOR shipkiller
+    
+                    # else:
+                    
+                    if(GoldRunnerRole.fitness(self, unit) > 0):
+                        print("CLAIMING: {} for GoldRunnerRole".format(unit.id))
+                        self.goals[unit.id] = GoldRunnerRole(self, unit)
+                    
+                    if(ShipKillerRole.fitness(self, unit) > 0):
+                        print("CLAIMING: {} for ShipKillerRole".format(unit.id))
+                        self.goals[unit.id] = ShipKillerRole(self, unit)
+
+
+
+            self.phase += 1            
+            return(False)
+        
+        elif(self.phase == 1):
+
+            for unit in self.player.units:
+                
+                goal = self.goals.get(unit.id, None)
+    
+                unit.log(
+                    "MINE" + 
+                    "goal: {}\n".format(goal) +
+                    # "acted: {}\n".format(unit.acted) +
+                    "crew: {}\n".format(unit.crew) +
+                    "crew_health: {}\n".format(unit.crew_health) +
+                    "gold: {}\n".format(unit.gold) +
+                    # "moves: {}\n".format(unit.moves) +
+                    # "owner: {}\n".format(unit.owner) +
+                    # "path: {}\n".format(unit.path) +
+                    "ship_health: {}\n".format(unit.ship_health) +
+                    # "stunturns: {}\n".format(unit.stunturns) +
+                    # "targetport: {}\n".format(unit.targetport) +
+                    # "tile: {}\n".format(unit.tile) +
+                    ""
+                    )
+    
+                # GOAL: shipkiller
+    
+                if(goal is not None):
+                    if(goal.execute()):
+                        self.goals[unit.id] = None
+    
+            self.phase += 1            
+            return(False)
+        
+
+
+
+        print("TRYING TO BUILD A CREW WITH {}/{} gold".format(self.player.gold, self.game.crew_cost))
+        if(
+            (self.player.port.tile.unit is None) and
+            (self.player.gold >= self.game.crew_cost)
+        ):
             self.player.port.spawn("crew")
-        elif self.player._units[0]._ship_health == 0:
+            print("BUILDING A CREW")
+    
+        print("TRYING TO BUILD A SHIP WITH {}/{} gold".format(self.player.gold, self.game.ship_cost))
+        while(
+            (self.player.gold >= self.game.ship_cost) and
+            (self.player.port.spawn("ship"))
+        ):
+            print("BUILDING A SHIP")
+            path = self.find_path(self.player.port.tile, self.player.opponent.port.tile, self.player.port.tile.unit)
+            if(len(path) > 0):
+                print("MOVING NEW SHIP TO {}, {}".format(path[0].x, path[0].y))
+                self.player.port.tile.unit.move(path[0])
+            
+        return(True)
+        
+
+        # for unit in self.game.units:            
+        #     unit.log(
+        #         "crew: {}\n".format(unit.crew) +
+        #         "crew_health: {}\n".format(unit.crew_health) +
+        #         "gold: {}\n".format(unit.gold) +
+        #         # "moves: {}\n".format(unit.moves) +
+        #         "owner: {}\n".format(unit.owner) +
+        #         #"path: {}\n".format(unit.path) +
+        #         "ship_health: {}\n".format(unit.ship_health) +
+        #         # "stunturns: {}\n".format(unit.stunturns) +
+        #         # "targetport: {}\n".format(unit.targetport) +
+        #         # "tile: {}\n".format(unit.tile) +
+        #         ""
+        #         )
+            
+        
+                
+                
+
+                # print("MERCHANT we are about to move to: {}".format(merchant.tile))
+                # # Find a path to this merchant
+                # if(self.move_to(unit, merchant.tile, self.game._ship_range)):
+            
+                #     unit.attack(merchant.tile, "ship")
+                #     unit.log("attacking: {}".format(merchant.tile))
+                    
+                # if(merchant.ship_health == 0):
+                #     goals[unit.id]["type"] = "unassigned"
+        
+
+            # GOAL: goldrunner
+            
+            # elif(goal["type"] == "goldrunner"):
+            #     if(self.move_to(unit, self.player.port.tile)):
+            #         unit.deposit()
+            #         goals[unit.id]["type"] = "unassigned"
+        
+        
+        # if len(self.player._units) == 0:
+            # Spawn a crew if we have no units
+        # elif self.player._units[0]._ship_health == 0:
             # Spawn a ship so our crew can sail
-            self.player.port.spawn("ship")
-        elif self.player._units[0]._ship_health < self.game._ship_health / 2.0:
-            # Heal our unit if the ship is almost dead
-            # Note: Crew also have their own health. Maybe try adding a check to see if the crew need healing?
-            unit = self.player._units[0]
 
-            # Find a path to our port so we can heal
-            path = self.find_path(unit.tile, self.player.port.tile, unit)
-            if len(path) > 0:
-                # Move along the path if there is one
-                unit.move(path[0])
-            else:
-                # Try to deposit any gold we have while we're here
-                unit.deposit()
+                
+        # elif self.player._units[0]._ship_health < self.game._ship_health / 2.0:
+        #     # Heal our unit if the ship is almost dead
+        #     # Note: Crew also have their own health. Maybe try adding a check to see if the crew need healing?
+        #     unit = self.player._units[0]
 
-                # Try to rest
-                unit.rest()
-        else:
-            # Try to attack a merchant
-            unit = self.player._units[0]
+        #     # Find a path to our port so we can heal
+        #     path = self.find_path(unit.tile, self.player.port.tile, unit)
+        #     if len(path) > 0:
+        #         # Move along the path if there is one
+        #         unit.move(path[0])
+        #     else:
+        #         # Try to deposit any gold we have while we're here
+        #         unit.deposit()
 
-            # Look for a merchant ship
-            merchant = None
-            for u in self.game.units:
-                if u._target_port is not None:
-                    # Found one
-                    merchant = u
-                    break
+        #         # Try to rest
+        #         unit.rest()
+        # else:
+            
 
-            # If we found a merchant, move to it, then attack it
-            if merchant is not None:
-                # Find a path to this merchant
-                path = self.find_path(unit.tile, merchant.tile, unit)
-                if len(path) > self.game._ship_range:
-                    # Move until we're within firing range of the merchant
-                    # Note: Range is *circular* in pirates, so this can be improved on
-                    unit.move(path[0])
-                else:
-                    # Try to attack the merchant's ship
-                    unit.attack(merchant.tile, "ship")
 
         return True
         # <<-- /Creer-Merge: runTurn -->>
+
+    def move_to(self, unit, goal, within = 0):
+        path = self.find_path(unit.tile, goal, unit)
+        i = 0
+        while (
+            (unit.moves > 0) and
+            (len(path) - within > i)
+        ):
+            unit.move(path[i])
+            i = i + 1
+            
+        if(len(path) - within <= i):
+            return True
+        else:
+            return False
 
     def find_path(self, start, goal, unit):
         """A very basic path finding algorithm (Breadth First Search) that when given a starting Tile, will return a valid path to the goal Tile.
@@ -125,6 +534,7 @@ class AI(BaseAI):
 
         # keep exploring neighbors of neighbors... until there are no more.
         while len(fringe) > 0:
+            
             # the tile we are currently exploring.
             inspect = fringe.pop(0)
 
